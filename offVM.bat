7ac6ec96f15a40743393bded3498b791f99685d0
Alex Serkin <nabeymordugoogly@gmail.com>
2026-08-08 21:43:40 +0300

    Update offVM.bat
    Добавлен комментарий для смены названия ВМ

--- offVM.bat
+++ offVM.bat
@@ -1,7 +1,8 @@
 @echo off
 echo [INFO] Shutting down virtual machine gracefully.
 "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm "UbuntuLocal" acpipowerbutton
-
+REM !!! ВНИМАНИЕ !!! "UbuntuLocal" заменить на название вашей ВМ 
+REM !!! АTTENTION !!!"UbuntuLocal" replace with the name of your VM
 if %errorlevel% equ 0 (
     echo [OK] Shutdown signal sent.
     echo [INFO] The VM will shut down gracefully, may take a few seconds.
